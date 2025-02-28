e7a846fa7c3181797b72d8e1b4b20182d598ef6a
Pavel <pba2028@yandex.ru>
2025-02-28 20:22:21 +0300

    Update api.js

--- api.js
+++ api.js
@@ -51,17 +51,48 @@ if (userId) {
     }
 }
 
+// async function saveProgress() {
+
+//     const progressData = {
+//         tg_id: getTelegramUserId(),          // Replace with the actual tg_id
+//         clickCount: localStorage.getItem('clickCount'),      // Replace with the actual coin number
+//         upgradeLevel: localStorage.getItem('upgradeLevel'),      // Replace with the actual click booster value
+//         hasAutoClicker: localStorage.getItem('hasAutoClicker')    // Replace with the actual auto clicker status (true/false)
+//       };
+
+//     try {
+//       const response = await fetch("http://localhost:8000/save_progress", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(progressData)
+//       });
+  
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`);
+//       }
+  
+//       const data = await response.json();
+//       console.log("Progress saved successfully:", data);
+//       // You can handle the response data as needed
+  
+//     } catch (error) {
+//       console.error("Ошибка при сохранении прогресса:", error);
+//       // Handle the error as needed, e.g., show a message to the user
+//     }
+//   }
+
 async function saveProgress() {
-
     const progressData = {
-        Progress: {
-          tg_id: getTelegramUserId(),
-          clickCount: localStorage.getItem('clickCount'),
-          upgradeLevel: localStorage.getItem('upgradeLevel'),
-          hasAutoClicker: localStorage.getItem('hasAutoClicker')
-        }
-      };
-
+      Progress: {
+        tg_id: getTelegramUserId(),          // Замените на фактический tg_id
+        clickCount: localStorage.getItem('clickCount'),      // Замените на фактическое количество кликов
+        upgradeLevel: localStorage.getItem('upgradeLevel'),      // Замените на фактический уровень улучшения
+        hasAutoClicker: localStorage.getItem('hasAutoClicker')    // Замените на фактический статус авто-кликера (true/false)
+      }
+    };
+  
     try {
       const response = await fetch("http://localhost:8000/save_progress", {
         method: "POST",
@@ -77,11 +108,7 @@ async function saveProgress() {
   
       const data = await response.json();
       console.log("Progress saved successfully:", data);
-      // You can handle the response data as needed
-  
     } catch (error) {
       console.error("Ошибка при сохранении прогресса:", error);
-      // Handle the error as needed, e.g., show a message to the user
     }
   }
-
