6b9dcf10811ececd524492750a0297656ef2a961
Pavel <pba2028@yandex.ru>
2025-03-04 13:02:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let count = 0;
-let energy = 100;
-const maxEnergy = 100;
-const energyRecoveryRate = 1;
-const recoveryInterval = 2000;
+// let energy = 100;
+// const maxEnergy = 100;
+// const energyRecoveryRate = 1;
+// const recoveryInterval = 2000;
 
 const counterDisplay = document.getElementById('counterDisplay');
-const energyLevel = document.getElementById('energyLevel');
-const energyText = document.getElementById('energyText');
+// const energyLevel = document.getElementById('energyLevel');
+// const energyText = document.getElementById('energyText');
 const coinButton = document.getElementById('coinButton');
 const upgradeButton = document.getElementById('upgradeButton');
 
@@ -23,6 +23,7 @@ function updateCounter() {
         counterDisplay.innerText = Math.floor(parseFloat(clickCount));
     }
 }
+
 
 // Function to add coins every X milliseconds
 function addCoins() {
@@ -51,7 +52,7 @@ if (localStorage.getItem('clickCount')) {
 }
 
 coinButton.addEventListener('click', () => {
-    if (energy > 0) {
+    if (useEnergy(0)) {
         let upgradeLevel = localStorage.getItem('upgradeLevel') || 0;
         let increment = parseInt(upgradeLevel) + 1;
 
@@ -73,18 +74,18 @@ coinButton.addEventListener('click', () => {
     }
 });
 
-function recoverEnergy() {
-    if (energy < maxEnergy) {
-        energy += energyRecoveryRate;
-        if (energy > maxEnergy) {
-            energy += energyRecoveryRate;
-        }
-        energyLevel.style.width = energy + '%';
-        energyText.textContent = energy;
-    }
-}
+// function recoverEnergy() {
+//     if (energy < maxEnergy) {
+//         energy += energyRecoveryRate;
+//         if (energy > maxEnergy) {
+//             energy += energyRecoveryRate;
+//         }
+//         energyLevel.style.width = energy + '%';
+//         energyText.textContent = energy;
+//     }
+// }
 
-setInterval(recoverEnergy, recoveryInterval);
+// setInterval(recoverEnergy, recoveryInterval);
 
 upgradeButton.addEventListener('click', () => {
     window.location.href = "upgrade.html";
